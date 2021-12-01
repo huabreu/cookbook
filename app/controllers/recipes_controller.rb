606a3c569d89b325534474c6e46599b2d101cab0
huabreu <hughra7@gmail.com>
2021-12-01 14:35:44 -0300

    formulary added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,24 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(name: params[:recipe][:name],
+                        recipe_type: params[:recipe][:recipe_type],
+                        cuisine: params[:recipe][:cuisine],
+                        ingredients: params[:recipe][:ingredients],
+                        cook_method: params[:recipe][:cook_method],
+                        cook_time: params[:recipe][:cook_time]
+                      )
+
+    if @recipe.save
+      return redirect_to recipe_path(@recipe.id)
+    end
+
+    render :new
+  end
 end
